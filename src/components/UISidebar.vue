3a244df85fa4b485f5565661dc75e061dd1df9d4
Diana <divnvp@yandex.com>
2022-05-18 21:01:33 +0900

    add: Sidebar component in Sidebar.vue

---
+++ src/components/UISidebar.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "UISidebar"
+};
+</script>
+
+<template>
+  <div>
+    <router-link to="/" class="ui-page__sidebar-link">
+      HOME
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./src/assets/style";
+</style>
