7d566f167582d56489fcf66b19b0215a61c97a7f
Diana <divnvp@yandex.com>
2022-05-18 21:00:35 +0900

    update: App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,3 @@
-<script setup>
-import HomePage from "./pages/HomePage.vue";
-</script>
-
 <template>
-  <HomePage />
+  <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
