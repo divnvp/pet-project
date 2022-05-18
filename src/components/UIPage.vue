baaefb3bafeafbc593e7c002dd4db90466484301
Diana <divnvp@yandex.com>
2022-05-18 20:59:37 +0900

    add: page component

---
+++ src/components/UIPage.vue
@@ -0,0 +1,20 @@
+<script>
+import UISidebar from "./UISidebar.vue";
+export default {
+  name: "UIPage",
+
+  components: { UISidebar }
+};
+</script>
+
+<template>
+  <div class="ui-page__main">
+    <u-i-sidebar class="ui-page__sidebar" />
+
+    <slot name="page" />
+  </div>
+</template>
+
+<style lang="scss">
+@import "./src/assets/style";
+</style>
