2230ca5201cc60c5c0c09291dbdf41975080629c
Diana <divnvp@yandex.com>
2022-05-18 21:56:48 +0900

    update: with linter rules

--- src/router/InstanceRouter.js
+++ src/router/InstanceRouter.js
@@ -2,10 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 
 const routes = [
-  { path: "/", component: HomePage },
+  { path: "/", component: HomePage, },
 ];
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
