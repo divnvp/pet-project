fb2e7f00d4caeadc0ee8c6cd217ba9c18bd992ec
Diana <divnvp@yandex.com>
2022-05-18 21:01:01 +0900

    add: Home page in HomePage.vue

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<script>
+import UIPage from "../components/UIPage.vue";
+import DailyImage from "../components/home/DailyImage.vue";
+
+export default {
+  name: "HomePage",
+
+  components: { UIPage, DailyImage }
+};
+</script>
+
+<template>
+  <UIPage>
+    <template #page>
+      <DailyImage />
+    </template>
+  </UIPage>
+</template>
