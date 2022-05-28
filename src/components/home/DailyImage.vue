8c25a8086969534d54201e439afa274203247ea3
Diana <divnvp@yandex.com>
2022-05-28 12:56:10 +0900

    update: daily image page with array of images' cards

--- src/components/home/DailyImage.vue
+++ src/components/home/DailyImage.vue
@@ -3,76 +3,85 @@ import { getDailyPictures } from "../../service/apod";
 import { ref } from "vue";
 
 import UILoader from "../UILoader.vue";
+import DateChooser from "./DateChooser.vue";
 
 export default {
   name: "DailyImage",
 
-  components: { UILoader, },
+  components: { DateChooser, UILoader, },
 
   setup() {
     let isLoading = ref(true);
 
-    const planetaryApod = ref({});
     const parameters = ref({
-      date: new Date().toISOString().substring(0, 10),
-      start_date: null,
-      end_date: null,
-      count: 1,
-      thumbs: false,
+      start_date: new Date(new Date() - 1000 * 60 * 60 * 24).toISOString().substring(0 ,10),
+      end_date: new Date().toISOString().substring(0 ,10),
     });
+
+    const planetaryApods = ref([]);
 
     const fetchDailyPicture = async () => {
       try {
         isLoading.value = true;
-        planetaryApod.value = (await getDailyPictures({ date: parameters.value.date, })).data;
+        planetaryApods.value = (await getDailyPictures(parameters.value)).data;
         isLoading.value = false;
       } catch (e) {
         alert(e.data.message);
       }
     };
 
+    const updateParams = (newParams) => {
+      parameters.value.start_date = newParams.start_date;
+      parameters.value.end_date = newParams.end_date;
+
+      fetchDailyPicture();
+    };
+
     fetchDailyPicture();
 
     return {
       isLoading,
-      planetaryApod,
+      planetaryApods,
+      updateParams,
     };
   },
 };
 </script>
 
 <template>
-  <div class="ui-page__slot">
-    <div class="main-img__card">
-      <div class="main-img__card-column">
-        <div class="main-img__card-row">
-          <div class="main-img__card-column">
-            <h2 class="main-img__card-title">{{ planetaryApod.title }}</h2>
+  <div>
+    <DateChooser @update-date="updateParams" />
+
+    <div v-for="pod in planetaryApods" :key="pod.date" class="main-img__card">
+      <div class="ui-column">
+        <div class="ui-row">
+          <div class="ui-column">
+            <h2 class="main-img__card-title">{{ pod.title }}</h2>
 
             <hr />
           </div>
         </div>
 
-        <div class="main-img__card-row">
-          <div class="main-img__card-column">
+        <div class="ui-row">
+          <div class="ui-column">
             <UILoader v-if="isLoading" />
 
             <img
-              v-if="!isLoading"
-              id="img"
-              :src="planetaryApod.url"
-              alt="Daily pic"
+                v-if="!isLoading"
+                id="img"
+                :src="pod.url"
+                alt="Daily pic"
             >
 
-            <span>{{ planetaryApod.date }}</span>
+            <span>{{ pod.date }}</span>
 
-            <span>{{ planetaryApod.copyright }}</span>
+            <span>{{ pod.copyright || "" }}</span>
           </div>
 
-          <div class="main-img__card-column">
-            <span class="main-img__card-explanation">
-              {{ planetaryApod.explanation }}
-            </span>
+          <div class="ui-column">
+          <span class="main-img__card-explanation">
+            {{ pod.explanation }}
+          </span>
           </div>
         </div>
       </div>
