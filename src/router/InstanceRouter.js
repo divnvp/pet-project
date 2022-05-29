2d2e6d42c932d4d907ce41329ab79d3e5a493e45
Diana <divnvp@yandex.com>
2022-05-29 14:38:51 +0900

    add: earth path to InstanceRouter.js

--- src/router/InstanceRouter.js
+++ src/router/InstanceRouter.js
@@ -1,8 +1,13 @@
+// Dependencies
 import { createRouter, createWebHistory } from "vue-router";
+
+// Components
 import HomePage from "../pages/HomePage.vue";
+import EarthPage from "../pages/EarthPage.vue";
 
 const routes = [
   { path: "/", component: HomePage, },
+  { path: "/earth", component: EarthPage, },
 ];
 
 export const router = createRouter({
